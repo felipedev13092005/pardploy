a10e67b9437f662f7f4a315ee36f4aabf91a363b
Juan Felipe Valbuena Valderrama <valbuenavalderramajuanfelipe9@gmail.com>
2026-05-06 12:38:56 -0500

    feat(backend): firsh code

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // Definimos las rutas
+    let app = Router::new()
+        .route("/", get(|| async { "🦁 Pardploy Engine rugiendo!" }))
+        .route("/health", get(|| async { "OK" }));
+
+    // Dirección donde escuchará el servidor (0.0.0.0 para Docker)
+    let addr = "0.0.0.0:8080";
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    
+    println!("🚀 Backend iniciado en http://{}", addr);
+
+    // Arrancamos el servidor
+    axum::serve(listener, app).await.unwrap();
 }
