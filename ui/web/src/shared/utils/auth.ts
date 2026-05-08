4b78b9865e2a1e75be1b773877724685440365a1
Juan Felipe Valbuena Valderrama <valbuenavalderramajuanfelipe9@gmail.com>
2026-05-08 17:56:18 -0500

    fix(ui/web/auth.ts): utilidad de auth

--- ui/web/src/shared/utils/auth.ts
+++ ui/web/src/shared/utils/auth.ts
@@ -1,11 +1,14 @@
 import { ApiService } from "./api-service";
-
+export interface UserAuth {
+  id: number,
+  username: string
+}
 export const AuthService = {
-  me: () => ApiService.get('/auth/me'),
+  me: () => ApiService.get<UserAuth>('/auth/me'),
   login: (username: string, password: string) =>
-    ApiService.post('/auth/login', { username, password }),
+    ApiService.post<UserAuth>('/auth/login', { username, password }),
   register: (username: string, password: string) =>
-    ApiService.post('/auth/register', { username, password }),
+    ApiService.post<UserAuth>('/auth/register', { username, password }),
   logout: () =>
     ApiService.post('/auth/logout', {}),
   refresh: () =>
