e5cba2642a4dc99d4a0a5bce38f5e155ae05ee9b
Juan Felipe Valbuena Valderrama <valbuenavalderramajuanfelipe9@gmail.com>
2026-05-08 17:57:01 -0500

    fix(web/MainProviders): impoment SidebarProvider

--- ui/web/src/shared/providers/MainProviders.tsx
+++ ui/web/src/shared/providers/MainProviders.tsx
@@ -1,13 +1,16 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Outlet } from "react-router"
+import { SidebarProvider } from "../ui/components/ui/sidebar"
 
 // Create a client
 const queryClient = new QueryClient()
 const MainProviders = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Outlet />;
-    </QueryClientProvider>
+    <SidebarProvider>
+      <QueryClientProvider client={queryClient}>
+        <Outlet />;
+      </QueryClientProvider>
+    </SidebarProvider>
   )
 }
 
